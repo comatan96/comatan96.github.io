34aee7b4cb05e1f3f5e81212e33d54d8ca23efa2
comatan96 <matanooshcohen@gmail.com>
2024-10-28 21:07:13 +0200

    updated resume data

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -11,9 +11,9 @@ export const RESUME_DATA = {
   location: "Earth",
   locationLink: null,
   about:
-    "Software Engineer driving impactful solutions for complex geo-spatial and data-driven challenges. Skilled in Python, Go, and graph theory, with a focus on scalable cloud architectures and optimized performance.",
+    "Software Engineer driving impactful solutions for complex geo-spatial and data-driven challenges. Skilled in Python, Go, and graph theory.",
   summary:
-  "Software Engineer with 5 years of experience tackling**complex data challenges**through graph theory and geo-spatial analysis. Skilled in designing efficient algorithms for**data processing and manipulation,**leading projects from concept to production. Experienced in mentoring teams, ensuring rigorous testing practices, and implementing robust logging for error management. Passionate about delivering the best solutions possible using cutting-edge technology to create**impactful results.**",
+  "Software Engineer with 5 years of experience delivering end-to-end solutions. Specializes in tackling complex data challenges through graph theory and geo-spatial analysis, optimizing algorithms for data processing. Led projects that improved outcomes by 60%, impacting large-scale real-world applications. Skilled in cross-functional collaboration, mentoring, and leveraging cutting-edge technology to drive impactful results. Eager to build solutions that reach hundreds of thousands of customers.",
   avatarUrl: "https://avatars.githubusercontent.com/u/34000779?s=400&u=9ac2e39dcb216332c2890f5a0480724ad9dfdae4&v=4",
   personalWebsiteUrl: "https://comatan96.github.io",
   contact: {
@@ -48,19 +48,19 @@ export const RESUME_DATA = {
       type: EMPLOYMENT_TYPES.FULL_TIME,
       link: "https://www.intel.com/content/www/us/en/homepage.html",
       badges: ["Python", "Go", "Data analysis", "Graph-theory", "Geospatial data", "CLIs"],
-      title: "Algorithms & Backend engineer",
+      title: "Software Engineer (Algorithms & Data)",
       logo: IntelLogo,
       start: "Aug, 2021",
       end: "Today",
       description:
-        "Designing and developing optimizations algorithms for graph-based problems using Python & Go.",
+        "Designing and developing data processing & mainpulation and optimizations algorithms for graph-based problems using Python & Go.",
       bullets: [
-        "**Applied graph theory to model complex GEO-based problems,** representing large datasets as **3D graphs** and developing advanced path optimization algorithms. **Achieved over 60% improvement** in quality during critical project phases and delivered these capabilities as a SaaS solution.",
-        "**Engineered a comprehensive SaaS solution, including a CLI that interfaces with the API endpoint and backend services for processing large structured datasets.**Designed services that create and analyze graphs, storing results and delivering an overall score (0-1) based on the analysis. This architecture improved data handling and enhanced decision-making capabilities.",
-        "**Designed and implemented a versatile library capable of reading diverse inputs, including large datasets and files that represent CPU layouts**for analysis & research (Deep Learning and Machine Learning). This library significantly enhanced data accessibility and improved the quality of insights for ongoing projects.",
-        "**Developed CLI applications for handling large datasets** delivered in various formats, primarily focused on**data analysis and manipulation**while ensuring seamless communication with other services. These tools streamlined workflows and significantly enhanced productivity in operational tasks.",
-        "**Modernized infrastructure by adopting internal cloud solutions with Kubernetes and Docker,** enhancing deployment flexibility and efficiency, and**achieving a 2x improvement** in development time for serveral key projects.",
-        "**Mentored and trained new team members,** conducted code reviews and demonstrated effective communication and collaboration skills within a team of 10-15 developers."
+        "**Developed a RESTful API for processing large, structured datasets representing graph topologies,**leveraging a**microservices architecture in Go for distributed analysis and scoring.**Enabled cloud-based assessment of design quality, improving the detection of inefficiencies and optimizing performance in scalable, data-intensive environments.",
+        "**Applied graph theory to model complex GEO-based problems, representing large datasets as graphs**and developing advanced path optimization algorithms.**Achieved over 60% improvement**in quality during critical project phases and delivered these capabilities as a**SaaS solution.**",
+        "**Designed and implemented a versatile python library**capable of reading diverse inputs, including**large datasets and files**that represent CPU layouts for**analysis & research (Deep Learning and Machine Learning).**This library significantly enhanced data accessibility and improved the quality of insights for ongoing projects",
+        "**Developed a CLI toolkit for processing large datasets delivered in various formats,**with a primary focus on data analysis and manipulation. Streamlined workflows by implementing efficient algorithms and optimized processing techniques,**resulting in an 80% reduction in runtimes**for large-scale procedures.",
+        "**Modernized infrastructure by transitioning multiple projects to cloud environments, adopting internal cloud solutions, Kubernetes, and Docker.**Enhanced deployment flexibility and efficiency, leading to a**2x improvement in development time**while ensuring smooth scalability and streamlined workflows.",
+        "**Mentored and trained new team members,** conducted code reviews and demonstrated effective communication and collaboration skills within a team of 8-10 developers."
       ]
     },
     {
