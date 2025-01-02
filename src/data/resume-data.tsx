40a826d988b7bc28da9437f756d71466f80a87da
Matan Cohen <34000779+comatan96@users.noreply.github.com>
2025-01-02 19:47:28 +0200

    update

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -13,7 +13,7 @@ export const RESUME_DATA = {
   about:
     "Software Engineer driving impactful solutions for complex geo-spatial and data-driven challenges. Skilled in Python, Go, and graph theory.",
   summary:
-  "Software Engineer with 5 years of experience delivering end-to-end solutions. Specializes in tackling complex data challenges through graph theory and geo-spatial analysis, optimizing algorithms for data processing. Led projects that improved outcomes by 60%, impacting large-scale real-world applications. Skilled in cross-functional collaboration, mentoring, and leveraging cutting-edge technology to drive impactful results. Eager to build solutions that reach hundreds of thousands of customers.",
+  "Software Engineer with 5 years of experience blending algorithm optimization and backend development. Specializes in solving complex data challenges through graph theory and scalable data processing. Enabled SaaS solutions by designing and implementing robust backend infrastructures, ensuring seamless cloud-based deployment and high-performance execution. Proficient in leading cross-functional teams, mentoring peers, and leveraging modern technology to deliver impactful software systems.",
   avatarUrl: "https://avatars.githubusercontent.com/u/34000779?s=400&u=9ac2e39dcb216332c2890f5a0480724ad9dfdae4&v=4",
   personalWebsiteUrl: "https://comatan96.github.io",
   contact: {
@@ -47,7 +47,7 @@ export const RESUME_DATA = {
       company: "Intel",
       type: EMPLOYMENT_TYPES.FULL_TIME,
       link: "https://www.intel.com/content/www/us/en/homepage.html",
-      badges: ["Python", "Go", "Data analysis", "Graph-theory", "Geospatial data", "CLIs"],
+      badges: ["Python", "Go", "Graph-theory", "Geospatial data", "CLIs", "Docker"],
       title: "Software Engineer (Algorithms & Data)",
       logo: IntelLogo,
       start: "Aug, 2021",
@@ -56,9 +56,9 @@ export const RESUME_DATA = {
         "Designing and developing data processing & mainpulation and optimizations algorithms for graph-based problems using Python & Go.",
       bullets: [
         "**Developed a RESTful API for processing large, structured datasets representing graph topologies,**leveraging a**microservices architecture in Go for distributed analysis and scoring.**Enabled cloud-based assessment of design quality, improving the detection of inefficiencies and optimizing performance in scalable, data-intensive environments.",
+        "**Designed and implemented a cloud-based system to calculate critical metrics, transforming complex workflows into scalable services.**Developed Dockerized environments and API endpoints to deliver key functionalities as a service, ensuring seamless integration and high performance. Built additional microservices to automate data transformations, streamlining processes and enhancing system interoperability.",
         "**Applied graph theory to model complex GEO-based problems, representing large datasets as graphs**and developing advanced path optimization algorithms.**Achieved over 60% improvement**in quality during critical project phases and delivered these capabilities as a**SaaS solution.**",
         "**Designed and implemented a versatile python library**capable of reading diverse inputs, including**large datasets and files**that represent CPU layouts for**analysis & research (Deep Learning and Machine Learning).**This library significantly enhanced data accessibility and improved the quality of insights for ongoing projects",
-        "**Developed a CLI toolkit for processing large datasets delivered in various formats,**with a primary focus on data analysis and manipulation. Streamlined workflows by implementing efficient algorithms and optimized processing techniques,**resulting in an 80% reduction in runtimes**for large-scale procedures.",
         "**Modernized infrastructure by transitioning multiple projects to cloud environments, adopting internal cloud solutions, Kubernetes, and Docker.**Enhanced deployment flexibility and efficiency, leading to a**2x improvement in development time**while ensuring smooth scalability and streamlined workflows.",
         "**Mentored and trained new team members,** conducted code reviews and demonstrated effective communication and collaboration skills within a team of 8-10 developers."
       ]
